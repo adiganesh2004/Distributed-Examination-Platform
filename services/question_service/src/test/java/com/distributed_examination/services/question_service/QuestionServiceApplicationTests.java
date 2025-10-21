40d317637b3ba896f01693771e6199ce7ca9661f
srivatsan-suresh <srivatsan.suresh@outlook.com>
2025-10-21 12:28:22 +0530

    Created question service

---
+++ services/question_service/src/test/java/com/distributed_examination/services/question_service/QuestionServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.distributed_examination.services.question_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestionServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
